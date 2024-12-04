1c6b070b196e60eb8b4c6318c3e71abed53c70b6
kngval <bkvalteocs@gmail.com>
2024-12-04 23:03:37 +0800

    Added scale,rect

--- Sprite.cs
+++ Sprite.cs
@@ -2,12 +2,33 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-class Sprite {
+class Sprite
+{
     public Texture2D _texture;
-    public Vector2 _position; 
+    public Vector2 _position;
 
-    public Sprite(Texture2D texture, Vector2 position){
+    public float SCALE = 5f;
+
+    public Rectangle Rect
+    {
+        get
+        {
+            return new Rectangle((int)_position.X, (int)_position.Y, _texture.Width * (int)SCALE, _texture.Height * (int)SCALE);
+        }
+    }
+    public Sprite(Texture2D texture, Vector2 position)
+    {
         _texture = texture;
         _position = position;
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+
+    }
+
+    public virtual void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, Rect, Color.White);
     }
 }
